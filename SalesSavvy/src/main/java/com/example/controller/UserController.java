9c2a42fa7dea0e2a6192eb3ef41b0a17b8497988
Syed Umar Mukthiyar <120204366+syedumar313@users.noreply.github.com>
2025-01-17 22:31:25 +0530

    Update UserController.java
    added java.util.* package

--- SalesSavvy/src/main/java/com/example/controller/UserController.java
+++ SalesSavvy/src/main/java/com/example/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.example.controller;
 
-import java.util.Map;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
