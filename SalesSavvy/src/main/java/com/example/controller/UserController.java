68e6735d654961757804c7904a77a036a9b7ea0b
aliyersyed166 <aliyersyed166@gmail.com>
2025-01-18 13:49:19 +0530

    Update UserController.java
    Let it be map only there is no need to download all the classes from util package

--- SalesSavvy/src/main/java/com/example/controller/UserController.java
+++ SalesSavvy/src/main/java/com/example/controller/UserController.java
@@ -1,6 +1,6 @@
 package com.example.controller;
 
-import java.util.*;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
